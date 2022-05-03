98cc6ac42be3f9ecf175934345b79a43ba2426e1
Filippo Nardon <filippo.nardon@studio.unibo.it>
2022-05-03 17:36:26 +0200

    feat: class Map and first map
    draft

--- src/includes/Map.hpp
+++ src/includes/Map.hpp
@@ -1,7 +1,19 @@
-class Map
-{
-    private:
 
+
+class Map{
+    protected:
+        int n_doors;
+        int walls[10][4];
     public:
         Map();
+};
+
+class map1 : public Map{
+    protected:
+    public:
+        map1() : Map(){
+            // walls[0][1] = {};
+            // walls[1] = 5;
+            // walls[2] = 
+        };
 };
